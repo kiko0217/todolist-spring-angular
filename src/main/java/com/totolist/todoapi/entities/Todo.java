f4afe811da778c91c6db89c0abefb2cb0b355e8d
kiko0217 <anziskita@gmail.com>
2022-07-21 23:25:05 +0700

    add merge mangement with flyway

---
+++ src/main/java/com/totolist/todoapi/entities/Todo.java
@@ -0,0 +1,28 @@
+package com.totolist.todoapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Table(name = "todo")
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "completed")
+    private Boolean completed;
+}
